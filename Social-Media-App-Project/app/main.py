9c261f422cce8a1a85cdae6b2eae3dbf0ddb9dee
Joel Joseph Thomas <joeljoseph26@yahoo.com>
2025-09-27 23:51:46 +0530

    database connection included

--- Social-Media-App-Project/app/main.py
+++ Social-Media-App-Project/app/main.py
@@ -3,6 +3,17 @@ from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
+import psycopg2
+from dotenv import load_dotenv
+from psycopg2.extras import RealDictCursor
+import os
+import time 
+
+load_dotenv()
+database_password = os.getenv("POSTGRES_PASSWORD")
+database_name = os.getenv("DATABASE_NAME")
+database_user = os.getenv("DATABASE_USER")
+database_host = os.getenv("DATABASE_HOST")
 
 app = FastAPI()
 
@@ -11,6 +22,19 @@ class Post(BaseModel):
     content: str
     published: bool = True
     rating: Optional[int] = None
+    
+while True:
+    try:
+        conn = psycopg2.connect(host=database_host, database=database_name, 
+                                user=database_user, password=database_password, 
+                                cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was successful!")
+        break
+    except Exception as error:
+        print("Connecting to database failed!")
+        print("Error: ",error)
+        time.sleep(5)
     
 my_posts = [{
     "title" : "Food",
